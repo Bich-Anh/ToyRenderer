a4789a242dc9af8468c39dd32569a30162921ed7
Bich-Anh H <dev@bkan.fr>
2024-12-16 15:39:17 +0100

    Adds Perlin Noise

--- include/texture.h
+++ include/texture.h
@@ -3,6 +3,7 @@
 
 #include "color.h"
 #include "common.h"
+#include "perlin.h"
 #include "rtw_stb_image.h"
 
 class texture
@@ -87,5 +88,22 @@ class image_texture : public texture
 
  private:
   rtw_image image;
+};
+
+class noise_texture : public texture
+{
+ private:
+  perlin noise;
+  double scale;
+
+ public:
+  noise_texture(double scale) : scale(scale) {}
+
+  color value(double u, double v, const point3& p) const override
+  {
+    //    return color(1,1,1) * 0.5 * (1.0 + noise.noise(scale * p));
+    return color(.5, .5, .5) *
+           (1 + std::sin(scale * p.z() + 10 * noise.turb(p, 7)));
+  }
 };
 #endif
